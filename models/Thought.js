f2a1e4e63be4084dcd8a3a96778cb0d57991e289
foxeyb28 <131126615+foxeyb28@users.noreply.github.com>
2023-10-09 23:07:33 -0500

    added code to index

--- models/Thought.js
+++ models/Thought.js
@@ -2,8 +2,13 @@ const { Schema, model } = require('mongoose');
 
 const thoughtSchema = new Schema(
     {
-        _id: '',
+        _id:{
+
+          _id: 'Id',
+        },
+        friends: '',
 
     }
+
 );
     module.exports = thoughtSchema;
