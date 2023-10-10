0a0ea07ae58bde444cfb64c1d69f63cd349ed1d1
foxeyb28 <131126615+foxeyb28@users.noreply.github.com>
2023-10-09 23:22:47 -0500

    added the controllers folder and files

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const {
+  getCourses,
+  getSingleCourse,
+  createCourse,
+  updateCourse,
+  deleteCourse,
+} = require('../../controllers/courseController.js');
+
+// /api/courses
+router.route('/').get(getThoguts).post(createThought);
+
+// /api/courses/:courseId
+router
+  .route('/:thoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+module.exports = router;
